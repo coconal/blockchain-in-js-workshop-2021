2b2ee4e47d5274466739b2951065b1f0757ce4fa
coconal <m15196712167@163.com>
2023-05-26 13:55:26 +0800

    branch lesson3 update

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,15 +1,46 @@
-import sha256 from 'crypto-js/sha256.js'
 
-export const DIFFICULTY = 2
+import sha256 from 'crypto-js/sha256.js'
+import UTXOPool from "./UTXOPool.js";
+export const DIFFICULTY = 3
 
 class Block {
   // 1. 完成构造函数及其参数
 
-  constructor() {}
 
-  isValid() {}
 
-  setNonce(nonce) {}
+
+  constructor(blockchain,previousHash,height,hash,coinbaseBeneficiary) {
+    this.blockchain = blockchain;
+    this.hash=hash
+    this.previousHash = previousHash.toString();
+    this.height = height;
+    this.coinbaseBeneficiary = coinbaseBeneficiary
+    this.utxoPool = new UTXOPool()
+  }
+
+  isValid() {
+    return  this.hash === this.calculateHash()&&
+        (this.hash.substring(0,DIFFICULTY) ==='0'.repeat(DIFFICULTY))
+  }
+
+  setNonce(nonce) {
+    this.nonce=nonce
+    this.setHash()
+  }
+
+  calculateHash(){
+    return sha256(
+        this.nonce+
+        this.previousHash+
+        this.height+
+        this.coinbaseBeneficiary
+    ).toString();
+  }
+  setHash(){
+    this.hash = this.calculateHash()
+  }
+
+
 }
 
 export default Block
